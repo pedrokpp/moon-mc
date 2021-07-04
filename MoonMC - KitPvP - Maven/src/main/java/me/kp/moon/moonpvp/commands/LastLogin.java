f9455a2c397b09c82bc7ef81f8ff1bc1ade70565
pedrokpp <pedrokpp9@gmail.com>
2021-07-04 16:38:12 -0300

    update: lastlogin agora funciona com players offline

--- MoonMC - KitPvP - Maven/src/main/java/me/kp/moon/moonpvp/commands/LastLogin.java
+++ MoonMC - KitPvP - Maven/src/main/java/me/kp/moon/moonpvp/commands/LastLogin.java
@@ -2,6 +2,7 @@ package me.kp.moon.moonpvp.commands;
 
 import me.kp.moon.moonpvp.enums.Messages;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -29,6 +30,15 @@ public class LastLogin implements CommandExecutor {
             }
             Player target = Bukkit.getPlayer(args[0]);
             if (target == null) {
+                Player targetOff;
+                for (OfflinePlayer p : Bukkit.getOfflinePlayers()) {
+                    if (p.getName().equalsIgnoreCase(args[0])) {
+                        Date date = new Date(p.getLastPlayed());
+                        player.sendMessage("§aO último login de §e" + p.getName() + " §afoi §7" +
+                                new SimpleDateFormat("dd/MM/yyyy").format(date) + " §aàs §7" + new SimpleDateFormat("HH:mm:ss").format(date)
+                                + "§a.");
+                    }
+                }
                 player.sendMessage("§cNão foi possível encontrar o player §e" + args[0] + "§c.");
                 return true;
             }
