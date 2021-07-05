943799a49306b9a3a749d7344060c7c9884fb3f2
pedrokpp <pedrokpp9@gmail.com>
2021-07-05 01:09:30 -0300

    adicionado: we playerdata

--- MoonMC - WorldEdit - Maven/src/main/java/me/kp/moon/worldedit/data/PlayerData.java
+++ MoonMC - WorldEdit - Maven/src/main/java/me/kp/moon/worldedit/data/PlayerData.java
@@ -1,6 +1,7 @@
 package me.kp.moon.worldedit.data;
 
 import lombok.Setter;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
@@ -10,6 +11,9 @@ public class PlayerData {
 
     private Player player;
     private UUID playerUUID;
+
+    public Location pos1;
+    public Location pos2;
 
     public PlayerData(Player player) {
         this.player = player;
