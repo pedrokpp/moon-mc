bbb81f12ba43f50a00d4ad17e6e31b6ca5c53966
pedrokpp <pedrokpp9@gmail.com>
2021-07-14 20:40:31 -0300

    adicionado: /money pay <player> <quantia>

--- MoonMC - KitPvP - Maven/src/main/java/me/kp/moon/moonpvp/commands/Money.java
+++ MoonMC - KitPvP - Maven/src/main/java/me/kp/moon/moonpvp/commands/Money.java
@@ -29,8 +29,37 @@ public class Money implements CommandExecutor {
             }
             Player target = Bukkit.getPlayer(args[0]);
             if (target == null) {
-                player.sendMessage("§cNão foi possível encontrar o player §e" + args[0] + "§c.");
-                return true;
+                if (!args[0].equalsIgnoreCase("pay")) {
+                    player.sendMessage("§cNão foi possível encontrar o player §e" + args[0] + "§c.");
+                    return true;
+                }
+                if (args.length < 3) {
+                    player.sendMessage(Messages.ARGUMENTOS_INSUFICIENTES.getUsage(label + " pay <player> <quantia>"));
+                    return true;
+                }
+                Player target1 = Bukkit.getPlayer(args[1]);
+                if (target1 == null) {
+                    player.sendMessage("§cO player §e" + args[1] + " §cnão foi encontrado online.");
+                    return true;
+                }
+                int quantia;
+                try {
+                    quantia = Integer.parseInt(args[2]);
+                } catch (NumberFormatException exception) {
+                    player.sendMessage("§cA quantia apresentada é inválida.");
+                    return true;
+                }
+                if (playerData.cacheCoins < quantia) {
+                    player.sendMessage("§cVocê não tem essa quantidade de moedas.");
+                    return true;
+                }
+                PlayerData targetData1 = PlayerDataManager.getPlayerData(target1);
+                if (targetData1 == null) return true;
+                playerData.setCacheCoins(playerData.cacheCoins - quantia);
+                targetData1.setCacheCoins(targetData1.cacheCoins + quantia);
+                player.sendMessage("§aVocê enviou §7$" + quantia + " para §7" + target1.getName() + "§a.");
+                target1.sendMessage("§aVocê recebeu §7$" + quantia + " de §7" + player.getName() + "§a.");
+                return false;
             }
             PlayerData targetData = PlayerDataManager.getPlayerData(target);
             if (targetData == null) return true;
