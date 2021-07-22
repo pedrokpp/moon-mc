52e53f30ccebb376250827d018d8169e45f2f800
pedrokpp <pedrokpp9@gmail.com>
2021-07-22 14:58:50 -0300

    moonlobby: npc spawning adicionado e location fix

--- MoonMC - Lobby - Maven/src/main/java/me/kp/moon/moonlobby/npc/NPC.java
+++ MoonMC - Lobby - Maven/src/main/java/me/kp/moon/moonlobby/npc/NPC.java
@@ -6,7 +6,9 @@ import org.bukkit.Location;
 
 @Getter
 public enum NPC {
-    KITPVP("§9§lKIT PVP", new Location(Bukkit.getWorlds().get(0), -46.5, 94, 0.5, -90, 0))
+    KITPVP("§9§lKIT PVP", new Location(Bukkit.getWorlds().get(0), -46.5, 94, 0.5, -90, 0)),
+    DUELS("§9§lDUELS", new Location(Bukkit.getWorlds().get(0), -49.5, 94, 10.5, -90, 0)),
+    HG("§9§lHG", new Location(Bukkit.getWorlds().get(0), -49.5, 94, -9.5, -90, 0)),
     ;
 
     private final String name;
--- MoonMC - Lobby - Maven/src/main/java/me/kp/moon/moonlobby/npc/NPCManager.java
+++ MoonMC - Lobby - Maven/src/main/java/me/kp/moon/moonlobby/npc/NPCManager.java
@@ -1,6 +1,7 @@
 package me.kp.moon.moonlobby.npc;
 
 import com.mojang.authlib.GameProfile;
+import me.kp.moon.moonlobby.data.PlayerData;
 import net.minecraft.server.v1_8_R3.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -15,21 +16,34 @@ import java.util.UUID;
 
 public class NPCManager {
 
-    public static void createNPC(Player player, Location location, String npcName) {
+    public static void createNPC(PlayerData playerData, Location location, NPC npcE) {
+        Player player = playerData.getPlayer();
         MinecraftServer nmsServer = ((CraftServer) Bukkit.getServer()).getServer();
         WorldServer nmsWorld = ((CraftWorld) Bukkit.getWorlds().get(0)).getHandle();
-        GameProfile npcProfile = new GameProfile(UUID.randomUUID(), npcName);
+        GameProfile npcProfile = new GameProfile(UUID.randomUUID(), npcE.getName());
 
         EntityPlayer npc = new EntityPlayer(nmsServer, nmsWorld, npcProfile, new PlayerInteractManager(nmsWorld));
-        Player npcPlayer = npc.getBukkitEntity().getPlayer();
-        npcPlayer.setPlayerListName("");
+
+        switch (npcE) {
+            case KITPVP:
+                playerData.setKitPvPEntityID(npc.getId());
+                break;
+            case DUELS:
+                playerData.setDuelsEntityID(npc.getId());
+                break;
+            default:
+                break;
+        }
+
         npc.setLocation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
 
         PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().playerConnection;
         playerConnection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, npc));
+        playerConnection.sendPacket(new PacketPlayOutEntityHeadRotation());
         playerConnection.sendPacket(new PacketPlayOutNamedEntitySpawn(npc));
+        playerConnection.sendPacket(new PacketPlayOutEntityHeadRotation(npc, (byte) (location.getYaw())));
+        playerConnection.sendPacket(new PacketPlayOutAnimation(npc, 0));
         playerConnection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, npc));
-
     }
 
 }
