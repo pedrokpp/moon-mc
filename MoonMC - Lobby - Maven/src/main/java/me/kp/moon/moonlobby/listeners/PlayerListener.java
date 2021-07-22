c23830cc3bab9444f0a37083dc4238d3c107c1f9
pedrokpp <pedrokpp9@gmail.com>
2021-07-22 14:00:34 -0300

    moonlobby: playerlistener adicionado

---
+++ MoonMC - Lobby - Maven/src/main/java/me/kp/moon/moonlobby/listeners/PlayerListener.java
@@ -0,0 +1,31 @@
+package me.kp.moon.moonlobby.listeners;
+
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class PlayerListener implements Listener {
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        event.setCancelled(true);
+    }
+
+}
