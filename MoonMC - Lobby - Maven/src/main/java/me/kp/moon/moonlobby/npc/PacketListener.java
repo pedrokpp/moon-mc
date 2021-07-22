17bea92915f9824c57295980db34060f1fc94c2d
pedrokpp <pedrokpp9@gmail.com>
2021-07-22 15:00:18 -0300

    moonlobby: npc interact mandando para os respectivos servidores

--- MoonMC - Lobby - Maven/src/main/java/me/kp/moon/moonlobby/npc/PacketListener.java
+++ MoonMC - Lobby - Maven/src/main/java/me/kp/moon/moonlobby/npc/PacketListener.java
@@ -5,6 +5,8 @@ import io.github.retrooper.packetevents.event.PacketListenerPriority;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 import io.github.retrooper.packetevents.packettype.PacketType;
 import io.github.retrooper.packetevents.packetwrappers.play.in.useentity.WrappedPacketInUseEntity;
+import me.kp.moon.moonlobby.data.PlayerData;
+import me.kp.moon.moonlobby.data.PlayerDataManager;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
@@ -17,21 +19,20 @@ public class PacketListener extends PacketListenerAbstract {
     @Override
     public void onPacketPlayReceive(PacketPlayReceiveEvent event) {
         Player player = event.getPlayer();
+        PlayerData playerData = PlayerDataManager.getPlayerData(player);
+        if (playerData == null) return;
         if (event.getPacketId() == PacketType.Play.Client.USE_ENTITY) {
             WrappedPacketInUseEntity wrappedPacketInUseEntity = new WrappedPacketInUseEntity(event.getNMSPacket());
-            WrappedPacketInUseEntity.EntityUseAction action = wrappedPacketInUseEntity.getAction();
+            if (wrappedPacketInUseEntity.getAction() == WrappedPacketInUseEntity.EntityUseAction.INTERACT_AT) return;
             Entity entity = wrappedPacketInUseEntity.getEntity(player.getWorld());
-            if (action == WrappedPacketInUseEntity.EntityUseAction.ATTACK ||
-                    action == WrappedPacketInUseEntity.EntityUseAction.INTERACT) {
-                if (entity != null) {
-                    //It is NOT an NPC if we can find the Bukkit Entity
-                    player.sendMessage("You have attacked an " + entity.getType());
-                } else {
-                    int entityID = wrappedPacketInUseEntity.getEntityId();
-                    player.sendMessage(wrappedPacketInUseEntity.getEntity().getCustomName());
-                    player.sendMessage(wrappedPacketInUseEntity.getEntity().getName());
-                    player.sendMessage("You have attacked a fake entity with the Entity ID: " + entityID);
-                }
+            if (entity == null) {
+                int entityID = wrappedPacketInUseEntity.getEntityId();
+                if (entityID == playerData.getKitPvPEntityID())
+                    player.chat("/server kitpvp");
+                else if (entityID == playerData.getDuelsEntityID())
+                    player.chat("/server duels");
+                else
+                    player.sendMessage("§cModo de jogo em desenvolvimento.");
             }
 
         }
