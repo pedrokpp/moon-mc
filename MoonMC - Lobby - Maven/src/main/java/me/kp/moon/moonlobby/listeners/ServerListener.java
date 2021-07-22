58ee617573a71938b22de384eddf20ed6de5bd0f
pedrokpp <pedrokpp9@gmail.com>
2021-07-22 14:31:34 -0300

    moonlobby: serverlistener velocity fix

--- MoonMC - Lobby - Maven/src/main/java/me/kp/moon/moonlobby/listeners/ServerListener.java
+++ MoonMC - Lobby - Maven/src/main/java/me/kp/moon/moonlobby/listeners/ServerListener.java
@@ -1,14 +1,27 @@
 package me.kp.moon.moonlobby.listeners;
 
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.server.ServerListPingEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
 
 public class ServerListener implements Listener {
 
     @EventHandler
-    public void onServerPing(ServerListPingEvent event) {
-        event.setMotd("motd!!!");
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (event.getTo().getBlock().getRelative(BlockFace.DOWN).getType() == Material.EMERALD_BLOCK) {
+            Player player = event.getPlayer();
+            Location loc = player.getLocation();
+            if (player.getGameMode() == GameMode.SURVIVAL)
+                player.setVelocity(new Vector(-4.3f, 1, 0));
+            player.playSound(loc, Sound.ORB_PICKUP, 6.0f, 1.0f);
+        }
     }
 
 }
