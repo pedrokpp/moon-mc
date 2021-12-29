cf9ac42cb0e5810dd37cffef9795721f6064e67c
kp <77463574+pedrokpp@users.noreply.github.com>
2021-12-29 11:52:29 -0300

    não executar como adm

--- setup-maven.bat
+++ setup-maven.bat
@@ -1,23 +1,34 @@
 @echo off
-title SCRIPT NÃO TESTADO CORRETAMENTE NO WINDOWS 
-echo [LOG] Checando existencia do PaperSpigot...
+title Setup Maven - BATCH (https://github.com/pedrokpp/moon-mc/)
+echo.
+echo [LOG] Checando permissoes...
 
+net session >nul 2>&1
+if %errorLevel% == 0 (
+  echo [ERRO] Nao execute o script como administrador
+  pause >nul
+  exit
+) else (
+     echo [OK] Executando sem permissoes de administrador
+)
+echo.
+echo [LOG] Checando existencia do PaperSpigot...
 if exist PaperSpigot_1.8.8.jar (
   echo [OK] PaperSpigot_1.8.8.jar
 ) else (
-  echo [ERR] PaperSpigot nao encontrado no diretorio atual
-  echo [ERR] Lembre-se de nomear o Paper como 'PaperSpigot_1.8.8.jar'
+  echo [ERRO] PaperSpigot nao encontrado no diretorio atual
+  echo [ERRO] Lembre-se de nomear o Paper como 'PaperSpigot_1.8.8.jar'
   pause >nul
   exit
 )
-
+echo.
 mvn install:install-file -Dfile=PaperSpigot_1.8.8.jar -DgroupId=com.destroystokyo.paper -DartifactId=paper-api -Dversion=1.8.8-R0.1-SNAPSHOT -Dpackaging=jar
 if %ERRORLEVEL% neq 0 goto erro
 
 pause >nul
 
 :erro
-echo [ERR] Maven não encontrado
+echo [ERRO] Maven não encontrado
 set /p input=[?] Gostaria de abrir o site oficial do Maven para instalar? (s/n) 
 if /I "%input%"=="s" goto yes
 exit
